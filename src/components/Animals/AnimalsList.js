4630f6709df770213c02413e558df5326c0cc795
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2021-11-02 14:00:16 +0200

    changed animalsList to class component

--- src/components/Animals/AnimalsList.js
+++ src/components/Animals/AnimalsList.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { animals } from './animals';
 import AnimalsCard from './AnimalsCard';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import AnimalSingle from "/Users/bc-macair007/Desktop/react_projects/animals/src/components/AnimalSingle.js"
 
-const AnimalsList = () => {
-    const match = useRouteMatch();
+class AnimalsList extends Component {
+    
+    animalsListing = animals.map((item) => (<AnimalsCard key={item.name} name={item.name} />));
 
-    const animalsListing = animals.map((item) => (<AnimalsCard key={item.name} name={item.name} />));
-
-
-    return (
-        <div className="animalsList">
-            <Switch>
-                <Route exact path={match.path}>
-                    {animalsListing}
-                </Route>
-                <Route path={`${match.path}/:animal`}>
-                    <AnimalSingle />
-                </Route>
-            </Switch>
-        </div>
-    );
-};
+    render() {
+        return (
+            <div className="animalsList">
+                <Switch>
+                    <Route exact path={this.props.match.path}>
+                        {this.animalsListing}
+                    </Route>
+                    <Route path={`${this.props.match.path}/:animal`}>
+                        <AnimalSingle />
+                    </Route>
+                </Switch>
+            </div>
+        );
+    }
+}
 
 export default AnimalsList;
