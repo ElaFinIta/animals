db65a93cc81f5ff5531634361600933155c591fd
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2021-12-14 13:01:21 +0200

    link on whole card

--- src/components/Animals/AnimalsCard.js
+++ src/components/Animals/AnimalsCard.js
@@ -9,9 +9,10 @@ const AnimalsCard = (props) => {
 
     return (
         <div className="card">
-            <h2>{props.name}</h2>
-            <img src={`https://source.unsplash.com/1600x900/?${props.name}`} alt={`${props.name}`}/>
-            <Link to={`${match.url}/${props.name}`}>Read more</Link>
+            <Link to={`${match.url}/${props.name}`}>
+                <h2>{props.name}</h2>
+                <img src={`https://source.unsplash.com/1600x900/?${props.name}`} alt={`${props.name}`}/>
+            </Link>
         </div>
     );
 };
